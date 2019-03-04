8f8b79d0d0422eb6958755699ced06608fc6bdb6
Eric Dowell <github@ericdowell.com>
2019-03-04 08:42:03 -0600

    Removing .idea folder.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import Endpoint from './src/endpoint'
 import CrudEndpoint from './src/crudEndpoint'
 import ResourceEndpoint from './src/resourceEndpoint'
 
-
 import ApiEndpoint from './src/apiEndpoint'
 import CrudApiEndpoint from './src/crudApiEndpoint'
 import ResourceApiEndpoint from './src/resourceApiEndpoint'
