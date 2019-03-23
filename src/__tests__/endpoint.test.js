5f82a35e43cc1ec45b9adaad5e17ecc1a55d60b4
Eric Dowell <github@ericdowell.com>
2019-03-23 15:44:59 -0500

    Switching tests to use snapshots.

--- src/__tests__/endpoint.test.js
+++ src/__tests__/endpoint.test.js
@@ -3,71 +3,51 @@ import Endpoint from '../endpoint'
 import MessageBag from '../errors/messageBag'
 
 test('the origin is localhost', () => {
-  const endpoint = new Endpoint()
-  expect(endpoint.origin).toStrictEqual('http://localhost')
+  expect(new Endpoint().origin).toMatchSnapshot()
 })
 
 test('the path is empty', () => {
-  const endpoint = new Endpoint()
-  expect(endpoint.path).toStrictEqual('')
+  expect(new Endpoint().path).toStrictEqual('')
 })
 
 test('the endpoint is empty', () => {
-  const endpoint = new Endpoint()
-  expect(endpoint.endpoint).toStrictEqual('')
+  expect(new Endpoint().endpoint).toStrictEqual('')
 })
 
 test('the getBaseUrl is localhost with trailing slash', () => {
-  const endpoint = new Endpoint()
-  expect(endpoint.getBaseUrl()).toStrictEqual('http://localhost/')
+  expect(new Endpoint().getBaseUrl()).toMatchSnapshot()
 })
 
 test('the queryOptions will return default options', () => {
-  const endpoint = new Endpoint()
-  const url = 'final/path'
-  const method = 'GET'
-  const options = endpoint.queryOptions(url, method)
-
-  expect(options.url).toStrictEqual(url)
-  expect(options.method).toStrictEqual(method)
-  expect(options.baseURL).toStrictEqual('http://localhost/')
-  expect(options.paramsSerializer).toBeDefined()
-  expect(typeof options.paramsSerializer).toStrictEqual('function')
-  expect(options.headers).toBeUndefined()
-  expect(options.data).toBeUndefined()
-  expect(Object.keys(options)).toEqual([
-    'url',
-    'method',
-    'baseURL',
-    'paramsSerializer'
-  ])
+  const options = new Endpoint().queryOptions('final/path', 'GET')
+  expect(options).toMatchSnapshot()
 })
 
 test('the responseData will return data key of object', () => {
-  const endpoint = new Endpoint()
-  const data = endpoint.responseData({ data: 'test' })
-
-  expect(data).toStrictEqual('test')
+  const data = new Endpoint().responseData({ data: 'test' })
+  expect(data).toMatchSnapshot()
 })
 
 test('the responseError will return default MessageBag', () => {
   const endpoint = new Endpoint()
-  const url = 'final/path'
-  const method = 'GET'
-  const message = 'failure to foo bar'
-  const options = endpoint.queryOptions(url, method)
-  const original = new Error(message)
-  const messageBag = endpoint.responseError(url, method, options, original)
+  const options = endpoint.queryOptions('final/path', 'GET')
+  const messageBag = endpoint.responseError(
+    options.url,
+    options.method,
+    options,
+    new Error('failure to foo bar')
+  )
 
   expect(messageBag).toBeInstanceOf(MessageBag)
-  expect(messageBag.message).not.toStrictEqual(message)
-  expect(messageBag.options).toStrictEqual(options)
-  expect(messageBag.original).toStrictEqual(original)
-  expect(messageBag.original.message).toStrictEqual(message)
+  expect(messageBag.message).not.toStrictEqual('failure to foo bar')
   expect(messageBag.message).toStrictEqual(messageBag.default)
-  expect(messageBag.url).toStrictEqual(url)
-  expect(messageBag.method).toStrictEqual(method)
-  expect(messageBag.method).toStrictEqual(method)
   expect(messageBag.getStatus()).toStrictEqual(messageBag.defaultStatus)
   expect(messageBag.hasMessage('test')).toStrictEqual(false)
+
+  expect(MessageBag.toString()).toMatchSnapshot()
+  expect(messageBag.options).toMatchSnapshot()
+  expect(messageBag.original.message).toMatchSnapshot()
+  expect(messageBag.original.toString()).toMatchSnapshot()
+  expect(messageBag.url).toMatchSnapshot()
+  expect(messageBag.method).toMatchSnapshot()
 })
