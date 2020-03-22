9dca3fc8f2d886555b4437bcf5ad4b522b20662d
Eric Dowell <github@ericdowell.com>
2020-03-22 13:34:51 -0500

    Using toHaveBeenLastCalledWith instead of toMatchSnapshot.

--- src/__tests__/crudEndpoint.test.ts
+++ src/__tests__/crudEndpoint.test.ts
@@ -22,11 +22,17 @@ describe(`${CrudEndpoint.name}`, (): void => {
     ['post'],
     ['put'],
   ])('the "%s" method calls request on parent', async(method): Promise<void> => {
-    expect.assertions(1)
+    expect.assertions(2)
     const instance = new CrudEndpoint()
     const request = jest.spyOn(instance, 'request').mockImplementation(async(): Promise<any> => true)
-    await callEndpoint(instance)[method]('path', { url: 'value' })
-    // eslint-disable-next-line jest/prefer-inline-snapshots
-    expect(request.mock.calls[0]).toMatchSnapshot()
+    const url = 'path'
+    const params = { filter: ['value'] }
+    await callEndpoint(instance)[method](url, params)
+    expect(request).toHaveBeenCalledTimes(1)
+    expect(request).toHaveBeenLastCalledWith(
+      url,
+      method,
+      params,
+    )
   })
 })
