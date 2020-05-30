72ef835736666780cfe2596f29907c041b782f0a
Eric Dowell <github@ericdowell.com>
2020-05-30 13:27:40 -0500

    Updating types and return types.

--- src/hooks/stateProvider.tsx
+++ src/hooks/stateProvider.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { node } from 'prop-types'
 // Internal
-import { StateActionCases, StateProviderHelpers, StateProviderProps } from './types'
+import { StateActionCases, StateProviderComponent, StateProviderHelpers, StateProviderProps } from './types'
 import { applyReducerState } from './helpers'
 
 export function generateStateProvider<S, R extends React.Reducer<any, any>>(
   Context: React.Context<S>,
   Reducer: [React.ReducerState<R>, React.Dispatch<React.ReducerAction<R>>],
   helpers?: StateProviderHelpers,
-): any {
+): StateProviderComponent<S> {
   function StateProvider(props: StateProviderProps): any {
     const Provider = Context.Provider as React.Provider<S & any>
     return (
@@ -29,31 +29,40 @@ export function generateStateProvider<S, R extends React.Reducer<any, any>>(
   return StateProvider
 }
 
-export function useStateProvider<S>(
+export function useContextReducer<S, R extends React.Reducer<any, any>>(
   initialState: S,
   actions: Record<string, any>,
-  cases?: StateActionCases,
+  cases?: StateActionCases<S>,
+): [React.Context<S>, [React.ReducerState<R>, React.Dispatch<React.ReducerAction<R>>]] {
+  return [
+    React.createContext<S>(initialState),
+    React.useReducer((prevState: any, action: any): any => {
+      if (!Object.values(actions).includes(action.type)) {
+        throw new Error(`Unknown action: "${action.type}"`)
+      }
+      if (typeof cases?.[action.type] !== 'function') {
+        return applyReducerState(prevState, action.type, action)
+      }
+      return cases[action.type](prevState, action)
+    }, initialState),
+  ]
+}
+
+export function useStateProvider<S, R extends React.Reducer<any, any>>(
+  initialState: S,
+  actions: Record<string, any>,
+  cases?: StateActionCases<S>,
   helpers?: StateProviderHelpers,
-): any {
-  const Context = React.createContext<S>(initialState)
-
-  const Reducer = React.useReducer((state: any, action: any): any => {
-    if (!Object.values(actions).includes(action.type)) {
-      throw new Error(`Unknown action: "${action.type}"`)
-    }
-    if (typeof cases?.[action.type] !== 'function') {
-      return applyReducerState(state, action.type, action)
-    }
-    return cases[action.type](action)
-  }, initialState)
-
-  const StateProvider = generateStateProvider<S, any>(Context, Reducer, {
-    ...helpers,
-    update: (type: any, payload: any): void => {
-      // dispatch
-      Reducer[1]({ type, [type]: payload })
-    },
-  })
-
-  return { Context, Reducer, StateProvider }
+): [React.Context<S>, StateProviderComponent<S>] {
+  const [Context, Reducer] = useContextReducer(initialState, actions, cases)
+  return [
+    Context,
+    generateStateProvider(Context, Reducer, {
+      update: (type: any, payload: any): void => {
+        // dispatch
+        Reducer[1]({ type, [type]: payload })
+      },
+      ...helpers,
+    }),
+  ]
 }
--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -6,8 +6,10 @@ export type FormChangeEvent = React.ChangeEvent<
 >
 
 // useStateProvider
-export type StateActionCases = { [actionType: string]: (action: any) => any }
+// TODO: Use React.Reducer<S, A> instead of custom type?
+export type StateActionCases<S> = { [actionType: string]: (state: S, action: any) => any }
+export type StateProviderComponent<S> = (props: StateProviderProps) => React.Provider<S & any>
 export type StateProviderHelpers = { [key: string]: (...args: any[]) => any }
 export interface StateProviderProps {
-  children: any
+  children: React.ReactNode | React.ReactNode[]
 }
