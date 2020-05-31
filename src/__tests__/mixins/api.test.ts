6cfbd6a0f65593883046e2e0bd89e9ea6f63bc0e
Eric Dowell <github@ericdowell.com>
2020-05-31 14:25:29 -0500

    Updating mixin tests to get coverage back.

--- src/__tests__/mixins/api.test.ts
+++ src/__tests__/mixins/api.test.ts
@@ -1,4 +1,3 @@
-import qs from 'qs'
 import { Endpoint } from '../../endpoint'
 import { ApiMixin } from '../../mixins'
 
@@ -6,89 +5,23 @@ class TestEndpoint extends ApiMixin(Endpoint) {}
 
 describe(`${ApiMixin.name}`, (): void => {
   it('values are inherited by Endpoint', (): void => {
-    expect.assertions(6)
+    expect.assertions(4)
     const endpoint = new TestEndpoint()
-    const headers = {
-      Accept: 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-    }
-    expect(endpoint.config).toStrictEqual({
-      headers,
-    })
+    expect(endpoint.config).toStrictEqual({})
     expect(endpoint.version).toBe('')
     expect(endpoint.path).toBe('api')
-    expect(endpoint.shouldStringify).toBe(true)
-    const data = 'string'
-    const stringify = jest.spyOn(qs, 'stringify').mockImplementation((): string => 'string')
-    expect(endpoint.requestConfig({ url: 'url', method: 'get', data })).toMatchInlineSnapshot(`
-      Object {
-        "baseURL": "http://localhost/api",
-        "data": "string",
-        "headers": Object {
-          "Accept": "application/json",
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        "method": "get",
-        "paramsSerializer": [Function],
-        "url": "url",
-      }
-    `)
-    expect(stringify).toHaveBeenCalledTimes(0)
-    stringify.mockRestore()
-  })
-
-  it('preventStringify set shouldStringify to false', (): void => {
-    expect.assertions(2)
-    const endpoint = new TestEndpoint()
-    expect(endpoint.shouldStringify).toBe(true)
-    endpoint.preventStringify()
-    expect(endpoint.shouldStringify).toBe(false)
-  })
-
-  it('calling preventStringify before requestConfig prevents call to qs.stringify', (): void => {
-    expect.assertions(2)
-    const endpoint = new TestEndpoint()
-    endpoint.preventStringify()
-    const stringify = jest.spyOn(qs, 'stringify').mockImplementation((): string => 'string')
-    const data = { foo: 'bar' }
-    expect(endpoint.requestConfig({ url: 'url', method: 'get', data })).toMatchInlineSnapshot(`
+    expect(endpoint.requestConfig({ url: 'test/url', method: 'get', data: { foo: 'bar' } }))
+      .toMatchInlineSnapshot(`
       Object {
         "baseURL": "http://localhost/api",
         "data": Object {
           "foo": "bar",
         },
-        "headers": Object {
-          "Accept": "application/json",
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
+        "headers": Object {},
         "method": "get",
         "paramsSerializer": [Function],
-        "url": "url",
+        "url": "test/url",
       }
     `)
-    expect(stringify).toHaveBeenCalledTimes(0)
-    stringify.mockRestore()
-  })
-
-  it('requestConfig calls qs.stringify if data is an object and not FormData', (): void => {
-    expect.assertions(2)
-    const endpoint = new TestEndpoint()
-    const stringify = jest.spyOn(qs, 'stringify').mockImplementation((): string => 'string')
-    const data = { foo: 'bar' }
-    expect(endpoint.requestConfig({ url: 'url', method: 'get', data })).toMatchInlineSnapshot(`
-      Object {
-        "baseURL": "http://localhost/api",
-        "data": "string",
-        "headers": Object {
-          "Accept": "application/json",
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        "method": "get",
-        "paramsSerializer": [Function],
-        "url": "url",
-      }
-    `)
-    expect(stringify).toHaveBeenCalledTimes(1)
-    stringify.mockRestore()
   })
 })
---
+++ src/__tests__/mixins/form.test.ts
@@ -0,0 +1,92 @@
+import qs from 'qs'
+import { Endpoint } from '../../endpoint'
+import { FormMixin } from '../../mixins'
+
+class TestEndpoint extends FormMixin(Endpoint) {}
+
+const data = { foo: 'bar' }
+
+describe(`${FormMixin.name}`, (): void => {
+  it('values are inherited by Endpoint', (): void => {
+    expect.assertions(5)
+    const endpoint = new TestEndpoint()
+    const headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    expect(endpoint.config).toStrictEqual({
+      headers,
+    })
+    expect(endpoint.path).toBe('')
+    expect(endpoint.shouldStringify).toBe(true)
+    const stringify = jest.spyOn(qs, 'stringify').mockImplementation((): string => 'string')
+    expect(endpoint.requestConfig({ url: 'url', method: 'get', data: 'string' })).toMatchInlineSnapshot(`
+      Object {
+        "baseURL": "http://localhost",
+        "data": "string",
+        "headers": Object {
+          "Accept": "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        "method": "get",
+        "paramsSerializer": [Function],
+        "url": "url",
+      }
+    `)
+    expect(stringify).toHaveBeenCalledTimes(0)
+    stringify.mockRestore()
+  })
+
+  it('preventStringify set shouldStringify to false', (): void => {
+    expect.assertions(2)
+    const endpoint = new TestEndpoint()
+    expect(endpoint.shouldStringify).toBe(true)
+    endpoint.preventStringify()
+    expect(endpoint.shouldStringify).toBe(false)
+  })
+
+  it('calling preventStringify before requestConfig prevents call to qs.stringify', (): void => {
+    expect.assertions(2)
+    const endpoint = new TestEndpoint()
+    endpoint.preventStringify()
+    const stringify = jest.spyOn(qs, 'stringify').mockImplementation((): string => 'string')
+    expect(endpoint.requestConfig({ url: 'url', method: 'get', data })).toMatchInlineSnapshot(`
+      Object {
+        "baseURL": "http://localhost",
+        "data": Object {
+          "foo": "bar",
+        },
+        "headers": Object {
+          "Accept": "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        "method": "get",
+        "paramsSerializer": [Function],
+        "url": "url",
+      }
+    `)
+    expect(stringify).toHaveBeenCalledTimes(0)
+    stringify.mockRestore()
+  })
+
+  it('requestConfig calls qs.stringify if data is an object and not FormData', (): void => {
+    expect.assertions(2)
+    const endpoint = new TestEndpoint()
+    const stringify = jest.spyOn(qs, 'stringify').mockImplementation((): string => 'string')
+    expect(endpoint.requestConfig({ url: 'url', method: 'get', data })).toMatchInlineSnapshot(`
+      Object {
+        "baseURL": "http://localhost",
+        "data": "string",
+        "headers": Object {
+          "Accept": "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        "method": "get",
+        "paramsSerializer": [Function],
+        "url": "url",
+      }
+    `)
+    expect(stringify).toHaveBeenCalledTimes(1)
+    stringify.mockRestore()
+  })
+})
