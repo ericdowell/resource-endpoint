3b9c9fc9e61db9a5e20b95f0cb75af7e158b7fa4
Eric Dowell <github@ericdowell.com>
2020-05-31 14:54:00 -0500

    Making 'url' arg required again.

--- src/endpoints/crud.ts
+++ src/endpoints/crud.ts
@@ -2,32 +2,32 @@ import { AxiosRequestConfig, AxiosResponse } from 'axios'
 import { Endpoint } from '../endpoint'
 
 export interface AxiosCrudEndpoint {
-  delete<T = any, R = AxiosResponse<T>>(url?: string, config?: AxiosRequestConfig): Promise<R>
-  get<T = any, R = AxiosResponse<T>>(url?: string, config?: AxiosRequestConfig): Promise<R>
-  patch<T = any, R = AxiosResponse<T>>(url?: string, config?: AxiosRequestConfig): Promise<R>
-  post<T = any, R = AxiosResponse<T>>(url?: string, config?: AxiosRequestConfig): Promise<R>
-  put<T = any, R = AxiosResponse<T>>(url?: string, config?: AxiosRequestConfig): Promise<R>
+  delete<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R>
+  get<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R>
+  patch<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R>
+  post<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R>
+  put<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R>
   request<T = any, R = AxiosResponse<T>>(requestConfig: AxiosRequestConfig): Promise<R>
 }
 
 export class CrudEndpoint extends Endpoint implements AxiosCrudEndpoint {
-  async delete<T = any, R = AxiosResponse<T>>(url?: string, config?: AxiosRequestConfig): Promise<R> {
+  async delete<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
     return this.request<T, R>({ ...config, url, method: 'delete' })
   }
 
-  async get<T = any, R = AxiosResponse<T>>(url?: string, config?: AxiosRequestConfig): Promise<R> {
+  async get<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
     return this.request<T, R>({ ...config, url, method: 'get' })
   }
 
-  async patch<T = any, R = AxiosResponse<T>>(url?: string, config?: AxiosRequestConfig): Promise<R> {
+  async patch<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
     return this.request<T, R>({ ...config, url, method: 'patch' })
   }
 
-  async post<T = any, R = AxiosResponse<T>>(url?: string, config?: AxiosRequestConfig): Promise<R> {
+  async post<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
     return this.request<T, R>({ ...config, url, method: 'post' })
   }
 
-  async put<T = any, R = AxiosResponse<T>>(url?: string, config?: AxiosRequestConfig): Promise<R> {
+  async put<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
     return this.request<T, R>({ ...config, url, method: 'put' })
   }
 }
