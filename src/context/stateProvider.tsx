ce306d6cc22e7abe072c0085279cf95856fccd6e
Eric Dowell <github@ericdowell.com>
2020-05-30 22:38:52 -0500

    Fixing type issues around Context/Reducer.

--- src/context/stateProvider.tsx
+++ src/context/stateProvider.tsx
@@ -4,46 +4,53 @@ import { node } from 'prop-types'
 import { StateAction, StateActionCases, StateProviderHelpers, StateProviderProps } from './types'
 import { applyReducerState } from './helpers'
 
-export function createStateProvider<S, R extends React.Reducer<any, any>>(
-  initialState: S,
-  actions: Record<string, string>,
-  options?: {
-    actionCases?: StateActionCases<S>
-    createProviderHelpers?: (dispatch: React.Dispatch<React.ReducerAction<R>>) => StateProviderHelpers
-  },
-): [React.Context<S>, (props: StateProviderProps) => React.ReactElement<React.ProviderProps<any>>] {
-  const Context = React.createContext<S>(initialState)
-
-  type ProviderProps = {
-    state: React.ReducerState<R> | S
+export function createStateProvider<S, R extends React.Reducer<any, any>>(options: {
+  initialState: S
+  actions: Record<string, string>
+  actionCases?: StateActionCases<S>
+  providerHelpers?: (dispatch: React.Dispatch<React.ReducerAction<R>>) => StateProviderHelpers
+}): [
+  React.Context<{
     dispatch: React.Dispatch<React.ReducerAction<R>>
-  } & Record<string, any>
+    helpers: StateProviderHelpers
+    state: S
+  }>,
+  (
+    props: StateProviderProps,
+  ) => React.ReactElement<
+    React.ProviderProps<{
+      dispatch: React.Dispatch<React.ReducerAction<R>>
+      helpers: StateProviderHelpers
+      state: S
+    }>
+  >,
+] {
+  type ProviderProps = {
+    dispatch: React.Dispatch<React.ReducerAction<R>>
+    helpers: StateProviderHelpers
+    state: S
+  }
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [state, dispatch] = React.useReducer((prevState: S, action: StateAction): any => {
+    if (!Object.values(options.actions).includes(action.type)) {
+      throw new Error(`Unknown action: "${action.type}"`)
+    }
+    if (typeof options?.actionCases?.[action.type] !== 'function') {
+      return applyReducerState(prevState, action)
+    }
+    return options.actionCases[action.type](prevState, action)
+  }, options.initialState)
+
+  const providerValue = {
+    helpers: typeof options?.providerHelpers === 'function' ? options.providerHelpers(dispatch) : {},
+    state,
+    dispatch,
+  }
+  const Context = React.createContext<ProviderProps>(providerValue)
 
   function StateProvider(props: StateProviderProps): React.ReactElement<React.ProviderProps<ProviderProps>> {
-    const [state, dispatch] = React.useReducer((prevState: S, action: StateAction): any => {
-      if (!Object.values(actions).includes(action.type)) {
-        throw new Error(`Unknown action: "${action.type}"`)
-      }
-      if (typeof options?.actionCases?.[action.type] !== 'function') {
-        return applyReducerState(prevState, action)
-      }
-      return options.actionCases[action.type](prevState, action)
-    }, initialState)
-
-    const Provider = Context.Provider as React.Provider<ProviderProps>
-    return (
-      <Provider
-        value={{
-          ...((typeof options?.createProviderHelpers === 'function' &&
-            options.createProviderHelpers(dispatch)) ||
-            {}),
-          state,
-          dispatch,
-        }}
-      >
-        {props.children}
-      </Provider>
-    )
+    return <Context.Provider value={providerValue}>{props.children}</Context.Provider>
   }
   StateProvider.propTypes = {
     children: node.isRequired,
