49b5dea66f119ad3292c60d322c88dabfac60eb0
Eric Dowell <github@ericdowell.com>
2020-08-22 18:37:50 -0500

    Adding options to useQuery.

--- src/helpers/useQuery.ts
+++ src/helpers/useQuery.ts
@@ -1,30 +1,40 @@
-import { CatchError, Query, QueryPayload } from './types'
 import { AxiosResponse } from 'axios'
 import { safeResponseData } from './safeResponseData'
+import { CatchError, Query, QueryPayload } from './types'
 
 const DEFAULT_CATCH: CatchError = (error: Error): never => {
   throw error
 }
 
-export function makeRequest<R>(payload: QueryPayload<R>, request: Query<R>, catchError: CatchError): void {
+export function makeRequest<R>(
+  payload: QueryPayload<R>,
+  request: Query<R>,
+  options: { isArray: boolean; catchError: CatchError },
+): void {
   request()
     .then((response: AxiosResponse) => {
-      const { errors, message, ...data } = safeResponseData<R | any>(response)
+      const { errors, message, ...data } = safeResponseData<R | any>(response, options.isArray)
       payload.errors = errors
       payload.message = message
       payload.data = data
       payload.loading = false
     })
-    .catch(catchError)
+    .catch(options.catchError)
 }
 
-export function useQuery<R>(request: Query<R>, catchError: CatchError = DEFAULT_CATCH): QueryPayload<R> {
+export function useQuery<R>(
+  request: Query<R>,
+  options: { isArray?: boolean; catchError?: CatchError },
+): QueryPayload<R> {
   const payload: QueryPayload<R> = {
     loading: true,
     message: undefined,
     errors: undefined,
     data: undefined,
   }
-  makeRequest(payload, request, catchError)
+  makeRequest(payload, request, {
+    catchError: options.catchError ?? DEFAULT_CATCH,
+    isArray: options.isArray ?? false,
+  })
   return payload
 }
