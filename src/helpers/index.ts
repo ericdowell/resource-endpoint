7f2b526aaf5587116bc291faa168215168f48552
Eric Dowell <github@ericdowell.com>
2020-08-22 19:06:20 -0500

    Updating export order and comment.

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,6 @@
 import { query } from './query'
 export * from './onSubmit'
+// Only export query, makeRequest is for internal testing/usage
+export { query }
 export * from './safeResponseData'
 export * from './types'
-// Only export useQuery, makeRequest is for internal testing/usage
-export { query }
