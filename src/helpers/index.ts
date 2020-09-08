d3aead4845ab8727fcad9ce98f02c81004d5e81f
Eric Dowell <github@ericdowell.com>
2020-09-07 20:36:39 -0500

    Updating dependencies and removing request heleprs.

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,1 @@
-// Only export query & submit, ./request/index is for internal usage only
-// export * from './request'
-export * from './request/query'
-export * from './request/submit'
-export * from './request/types'
 export * from './safeResponseData'
